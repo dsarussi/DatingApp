11480a645ef77f8a4a084ccf867924841f40736d
Dor Sarussi <dor.sarussi@gmail.com>
2023-02-14 13:42:33 +0200

    end of section 14

---
+++ API/Helpers/LikesParams.cs
@@ -0,0 +1,8 @@
+namespace API.Helpers
+{
+    public class LikesParams : PaginationParams
+    {
+        public int userId { get; set; }
+        public string Predicate { get; set; }
+    }
+}
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { take } from 'rxjs/operators';
 import { Member } from 'src/app/_models/member';
 import { Pagination } from 'src/app/_models/pagination';
-import { User } from 'src/app/_models/user';
 import { UserParams } from 'src/app/_models/userParams';
-import { AccountService } from 'src/app/_services/account.service';
 import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
@@ -17,8 +13,8 @@ export class MemberListComponent implements OnInit {
   // members$: Observable<Member[]> | undefined;
   members: Member[] = [];
   pagination: Pagination | undefined;
-  userParams: UserParams| undefined;
-  genderList = [{value: 'male', display: 'Males'}, {value: 'female', display: 'Females'}]
+  userParams: UserParams | undefined;
+  genderList = [{ value: 'male', display: 'Males' }, { value: 'female', display: 'Females' }]
 
   constructor(private memberService: MembersService) {
     this.userParams = this.memberService.getUserParams();
@@ -29,12 +25,12 @@ export class MemberListComponent implements OnInit {
     this.loadMembers();
   }
 
-  loadMembers(){
-    if(this.userParams) {
+  loadMembers() {
+    if (this.userParams) {
       this.memberService.setUserParams(this.userParams);
       this.memberService.getMembers(this.userParams).subscribe({
         next: response => {
-          if(response.result && response.pagination) {
+          if (response.result && response.pagination) {
             this.members = response.result;
             this.pagination = response.pagination;
           }
@@ -43,13 +39,13 @@ export class MemberListComponent implements OnInit {
     }
   }
 
-  resetFilters(){
-      this.userParams = this.memberService.resetUserParams();
-      this.loadMembers();
+  resetFilters() {
+    this.userParams = this.memberService.resetUserParams();
+    this.loadMembers();
   }
 
-  pageChanged(event: any){
-    if(this.userParams && this.userParams?.pageNumber !== event.page) {
+  pageChanged(event: any) {
+    if (this.userParams && this.userParams?.pageNumber !== event.page) {
       this.userParams.pageNumber = event.page;
       this.memberService.setUserParams(this.userParams);
       this.loadMembers();
